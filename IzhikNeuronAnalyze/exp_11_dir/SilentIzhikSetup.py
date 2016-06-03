76ec49eedcf61ba4016c80fe17a2b8158134d4e5
Arjun Rao <arjun210493@gmail.com>
2016-06-03 18:18:51 +0530

    Refactored CMake Call into interface provided in CMakeModules
    Changes:
    
    1.  Added CMakeModules to repository
    
    2.  Changed Python setup files to use the new interface

--- IzhikNeuronAnalyze/exp_11_dir/SilentIzhikSetup.py
+++ IzhikNeuronAnalyze/exp_11_dir/SilentIzhikSetup.py
@@ -5,8 +5,6 @@ import os
 from RepoManagement.BasicUtils import changedDir, getFrameDir
 from RepoManagement import getRootDirectory
 from RepoManagement import SubModuleProcessing as SMP
-import subprocess
-import shlex
 
 # get Current Script Directory
 CurrentScriptDir = getFrameDir()
@@ -20,13 +18,15 @@ SubModulesDir = os.path.join(ExperimentRootDir, 'SubModulesDir')
 GetAttractionBasinDir = os.path.join(SubModulesDir, 'GetAttractionBasin')
 
 # Initialize Directories relevant to CMake
-CMakeModulesDir = os.path.join(GetAttractionBasinDir, 'CMakeModules')
+CMakeModulesDir = os.path.join(SubModulesDir, 'CMakeModules')
 CurrentSourceDir = GetAttractionBasinDir
 BuildDir = os.path.join(CurrentScriptDir, 'build')
 
 # add CMakeModulesDir to sys path and get default platform
 sys.path.insert(0, CMakeModulesDir)
 from CMakePyHelper import getDefaultPlatformGen
+from CMakePyHelper import CMakeGenCall, CMakeBuildCall
+
 DefaultGenerator = getDefaultPlatformGen()
 
 # Initialize Path to the HPP file correspondingto the dynamic system.
@@ -35,38 +35,13 @@ if not os.path.isdir(BuildDir):
     os.mkdir(BuildDir)
 
 with changedDir(BuildDir):
-    CMakeCmd = (
-        'cmake -G "{Generator}" '.format(Generator=DefaultGenerator) +
-        '-D CMAKE_BUILD_TYPE=Release ' +
-        '-D DYN_SYSTEM_HPP_PATH={DynSystemHPPPath} '.format(DynSystemHPPPath=DynSystemHPPPath) +
-        '"{SourceDir}"'.format(SourceDir=CurrentSourceDir))
-
-    with open(os.devnull, 'w') as NullStream:
-        CMakeOutput = subprocess.call(shlex.split(CMakeCmd), stdout=NullStream)
-
-    print(CMakeOutput)
-    if not CMakeOutput:
-        print("\nCMake build files successfully generated")
-        isCMakeGenSuccess = True
-    else:
-        print("\nErrors occurred in the execution of the following command:")
-        print()
-        print("  {Command}".format(Command=CMakeCmd))
-        isCMakeGenSuccess = False
+    isCMakeGenSuccess = CMakeGenCall(CurrentSourceDir,
+                                     Generator=DefaultGenerator,
+                                     BuildConfig='Release',
+                                     Silent=True,
+                                     DYN_SYSTEM_HPP_PATH=DynSystemHPPPath)
 
 if isCMakeGenSuccess:
-    CMakeBuildCmd = 'cmake --build "{BuildDir}" --target install --config Release'.format(
-        BuildDir=BuildDir)
-    
-    with open(os.devnull, 'w') as NullStream:
-        CMakeBuildOutput = subprocess.call(shlex.split(CMakeBuildCmd), stdout=NullStream)
-    
-    if not CMakeBuildOutput:
-        print("\nCMake build and install successfully completed")
-        print("Installed in:")
-        print("\n  {Directory}".format(Directory=os.path.join(BuildDir, 'install')))
-    else:
-        print("\nErrors occurred in the execution of the following command:")
-        print()
-        print("  {Command}".format(Command=CMakeBuildCmd))
-        isCMakeGenSuccess = False
+    CMakeBuildSuccess = CMakeBuildCall(BuildDir,
+                                       Target='install',
+                                       BuildConfig='Release')
