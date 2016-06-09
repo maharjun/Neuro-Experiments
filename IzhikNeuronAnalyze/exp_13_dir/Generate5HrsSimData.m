94d322da405e66397cee3fd5343f02380c2d5263
Arjun Rao <arjun210493@gmail.com>
2016-06-10 03:00:45 +0530

    Made Code Prettier: Exp 13

--- IzhikNeuronAnalyze/exp_13_dir/Generate5HrsSimData.m
+++ IzhikNeuronAnalyze/exp_13_dir/Generate5HrsSimData.m
@@ -1,7 +1,7 @@
 WorkingMemModulePath = deblank(fileread('./TimeDelNetSim_build/install/ModulePath.txt'));
 addpath(fullfile(WorkingMemModulePath, 'TimeDelNetSim', 'MatlabSource'))
 
-%%
+%% Setup Network Parameters
 rng('default');
 rng(25);
 N = 1000;
@@ -28,40 +28,48 @@ a(Ninh) = 0.1;
 b(Ninh) = 0.2;
 c(Ninh) = -65;
 d(Ninh) = 2;
-% Delays = Delays + 10;
 [NEndVect, NStartVect] = find(A);
+
+%% Running Simulation to generate sparse representation of 5 Hrs Simulation
 
 OutputOptions = {'FSF', 'Initial'};
 % Clearing InputStruct
 clear InputStruct;
 
-% Getting Midway state
+% Neuron Parameters
 InputStruct.a = single(a);
 InputStruct.b = single(b);
 InputStruct.c = single(c);
 InputStruct.d = single(d);
 
-InputStruct.NStart = int32(NStartVect);
-InputStruct.NEnd   = int32(NEndVect);
+% Network Parameters
+InputStruct.NStart              = int32(NStartVect);
+InputStruct.NEnd                = int32(NEndVect);
 InputStruct.InitialState.Weight = single(Weights);
-InputStruct.Delay  = single(Delays);
+InputStruct.Delay               = single(Delays);
+InputStruct.DelayRange          = int32(WorkingMemNetParams.DelayRange);
 
-InputStruct.V = single(-65*ones(N,1));
-InputStruct.U = single(0.2*InputStruct.V);
+% Simulation Parameters
+InputStruct.onemsbyTstep = int32(1);
+InputStruct.NoOfms       = int32(5*60*60*1000);
 
-InputStruct.onemsbyTstep                   = int32(1);
-InputStruct.NoOfms                         = int32(5*60*60*1000);
-InputStruct.DelayRange                     = int32(WorkingMemNetParams.DelayRange);
-InputStruct.StorageStepSize                = int32(60*1000);
-InputStruct.OutputControl                  = strjoin(OutputOptions);
-InputStruct.StatusDisplayInterval          = int32(4000);
+% Interfacing Parameters
+InputStruct.StorageStepSize       = int32(60*1000);
+InputStruct.OutputControl         = strjoin(OutputOptions);
+InputStruct.StatusDisplayInterval = int32(4000);
+
+% External Current Parameters
+InputStruct.Iext.IExtAmplitude             = single(0);
+InputStruct.Iext.IRandAmplitude            = single(30);
+InputStruct.Iext.AvgRandSpikeFreq          = single(1);
 InputStruct.InitialState.Iext.IExtGenState = uint32(30);
 
+% LT-STDP Related Parameters
 InputStruct.MaxSynWeight                   = single(8);
+
+% ST-STDP Related Parameters
 InputStruct.ST_STDP_EffectMaxCausal        = single(0.0);
 InputStruct.ST_STDP_EffectMaxAntiCausal    = single(0.0);
-InputStruct.Iext.IExtAmplitude             = single(0);
-InputStruct.Iext.AvgRandSpikeFreq          = single(1);
 
 if ~exist('TimeDelNetSim_build/Data', 'dir')
     mkdir('TimeDelNetSim_build/Data')
