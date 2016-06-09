c57efd7af08346b1ee828cf2e95e59340725e0a5
Arjun Rao <arjun210493@gmail.com>
2016-06-10 02:57:15 +0530

    Increased Range + Added Obs & Inf: Exp 10

--- IzhikNeuronAnalyze/exp_10_dir/IzhikNeuronAttBasin.m
+++ IzhikNeuronAnalyze/exp_10_dir/IzhikNeuronAttBasin.m
@@ -7,8 +7,8 @@ DynSysParameters.b = single(0.2);
 DynSysParameters.c = single(-65);
 DynSysParameters.d = single(8);
 
-DynSysParameters.GridXSpec = single([-5; 0.05; 35]);
-DynSysParameters.GridYSpec = single([-80; 0.05; 30]);
+DynSysParameters.GridXSpec = single([-25; 0.02; 35]);
+DynSysParameters.GridYSpec = single([-100; 0.02; 30]);
 
 DynSysParameters.onemsbyTstep = uint32(2);
 
@@ -17,7 +17,7 @@ DynSysParameters.onemsbyTstep = uint32(2);
 
 %% Plot
 IzhikPartitionFig = figure();
-colormap(IzhikPartitionFig, copper(length(IzhikBasinPartitionBoundaries)));
+colormap(IzhikPartitionFig, colorcube(length(IzhikBasinPartitionBoundaries)));
 GridXSpec = DynSysParameters.GridXSpec; % [0 1 1];
 GridYSpec = DynSysParameters.GridYSpec; % [0 1 1];
 fill(double(IzhikFullBasinBoundary.X)*GridXSpec(2) + GridXSpec(1), double(IzhikFullBasinBoundary.Y)*GridYSpec(2) + GridYSpec(1), 'b', 'LineStyle', 'none');
@@ -30,7 +30,7 @@ xlim([GridXSpec(1), GridXSpec(3)])
 ylim([GridYSpec(1), GridYSpec(3)])
 
 SimpIzhikPartitionFig = figure();
-colormap(SimpIzhikPartitionFig, copper(length(SimpIzhikBasinPartitionBoundaries)));
+colormap(SimpIzhikPartitionFig, colorcube(length(SimpIzhikBasinPartitionBoundaries)));
 fill(double(SimpIzhikFullBasinBoundary.X)*GridXSpec(2) + GridXSpec(1), double(SimpIzhikFullBasinBoundary.Y)*GridYSpec(2) + GridYSpec(1), 'b', 'LineStyle', 'none');
 hold on;
 for i = 1:length(SimpIzhikBasinPartitionBoundaries)
