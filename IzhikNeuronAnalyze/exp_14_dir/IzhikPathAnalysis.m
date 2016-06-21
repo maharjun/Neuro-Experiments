2a692ed87b92195d4a6d44586b69b8b540982263
Arjun Rao <arjun210493@gmail.com>
2016-06-22 00:39:00 +0530

    Added Trip Approximation + BUGFIX + Changes Exp 14

--- IzhikNeuronAnalyze/exp_14_dir/IzhikPathAnalysis.m
+++ IzhikNeuronAnalyze/exp_14_dir/IzhikPathAnalysis.m
@@ -1,7 +1,7 @@
 %% StreamLine Plot
 
-a = InputStateSparse.a(1);
-b = InputStateSparse.b(1);
+a = 0.02;
+b = 0.2;
 
 [U, V] = meshgrid(-20:0.5:15, -100:8:30);
 
@@ -11,7 +11,7 @@ figure;
 hold on;
 for i = 1:size(U, 2)
     for j = 1:size(U, 1)
-        [LineU, LineV] = getIzhikLineofConv(a,b,[U(j,i), V(j,i)], onemsbyTstep);
+        [LineU, LineV] = getIzhikTrajectory(a,b,[U(j,i), V(j,i)], onemsbyTstep);
         plot(LineU, LineV);
     end
     if (mod(U(1,i), 5) == 0)
@@ -30,8 +30,8 @@ end
 % quiver(U, V, dU, dV);
 
 %% Testing some trajectories and approximation
-a = InputStateSparse.a(1);
-b = InputStateSparse.b(1);
+a = 0.02;
+b = 0.2;
 
 InitPoint1 = [40, -50];
 InitPoint2 = [20, -50];
@@ -47,11 +47,13 @@ U4 = U1;
 % Eqn = 0.04V^2 + 5V + 140 - U = 0
 % => V = (-5 - sqrt(25 - 4*0.04*(140-U)))*12.5
 V4 = (-5 - sqrt(25-4*0.04*(140-U4)))*12.5;
+U5 = U1;
+V5 = (-5 + sqrt(25-4*0.04*(140-U4)))*12.5;
 
-figure;
 plot(U1, V1);
 hold on;
 plot(U2, V2);
 plot(U3, V3);
 plot(U4, V4);
+plot(U5, V5);
 hold off;
